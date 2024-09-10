990f09cc376ad00c95c2ba4f30ad9991c5e56a4d
dreamcar <mustafaguseiinov@gmail.com>
2024-09-10 17:09:05 +0300

    add main structs

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	todo "github.com/mguseinovv/todoApp"
+	"log"
+)
 
+func main() {
+	server := new(todo.Server)
+	if err := server.Run(8000); err != nil {
+		log.Fatalf("Failed to start server %s", err.Error())
+	}
 }
